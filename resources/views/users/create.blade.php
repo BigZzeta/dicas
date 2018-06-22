52e0028d42728ac1ebb47e9cbf913c02bb404240
Eric <ericcenteno1@gmail.com>
2018-06-21 23:13:33 -0500

    Actualizado

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -10,46 +10,7 @@
   <form class="form-grup" method="POST" action="{{ url('usuarios/crear') }}">
     <!--{!! csrf_field() !!}-->
 
-    {{ csrf_field() }}
-
-    <!--
-    <section class="content">
-      <div class="row">
-        <div class="col-md-6">
-
-    <table class="table table-bordered">
-      <tbody>
-        <tr>
-          <td width="20%"><label for="name">Nombre:</label></td>
-          <td><input type="text" class="form-control my-colorpicker1" name="name" id="name" placeholder="nombre"></td>
-        </tr>
-        <tr>
-          <td><label for="apellidoPaterno">Apellido Paterno:</label></td>
-          <td><input type="text" name="apellidoPaterno" id="apellidoPaterno" placeholder="Apellido Paterno"></td>
-        </tr>
-        <tr>
-          <td><label for="apellidoMaterno">Apellido Materno:</label></td>
-          <td><input type="text" name="apellidoMaterno" id="apellidoMaterno" placeholder="Apellido Materno"></td>
-        </tr>
-        <tr>
-          <td><label for="email">Email:</label></td>
-          <td><input type="email" name="email" id="email" placeholder="correo"></td>
-        </tr>
-        <tr>
-          <td><label for="username">Nombre de Usuario:</label></td>
-          <td><input type="text" name="username" id="username"  placeholder="nombre de usuario"></td>
-        </tr>
-        <tr>
-          <td><label for="password">Contraseña:</label></td>
-          <td><input type="password" name="password" id="password" placeholder="********"></td>
-        </tr>
-      </tbody>
-    </table>
-
-  </div>
-
-</div>-->
-
+    {{ csrf_field() }}  
 
     </section>
         <!-- Main content -->
@@ -71,35 +32,35 @@
                 <div class="box-body">
                   <div class="form-group">
                     <label>Apellido Paterno:</label>
-                    <input type="text" class="form-control my-colorpicker1" name="apellidoPaterno" placeholder="Apellido Paterno" required>
+                    <input type="text" class="form-control my-colorpicker1" name="apellidoPaterno" placeholder="Apellido Paterno">
                   </div>
                 </div>
                 <!--Apellido Paterno -->
                 <div class="box-body">
                   <div class="form-group">
                     <label>Apellido Materno:</label>
-                    <input type="text" class="form-control my-colorpicker1" name="apellidoMaterno" placeholder="Apellido Materno" required>
+                    <input type="text" class="form-control my-colorpicker1" name="apellidoMaterno" placeholder="Apellido Materno">
                   </div>
                 </div>
                 <!--email -->
                 <div class="box-body">
                   <div class="form-group">
                     <label>Email:</label>
-                    <input type="email" class="form-control my-colorpicker1" name="email" placeholder="Correo Electronico" required>
+                    <input type="email" class="form-control my-colorpicker1" name="email" placeholder="Correo Electronico">
                   </div>
                 </div>
                 <!--Nombre de Usuario -->
                 <div class="box-body">
                   <div class="form-group">
                     <label>Nombre de Usuario:</label>
-                    <input type="text" class="form-control my-colorpicker1" name="username" placeholder="Nombre de usuario" required>
+                    <input type="text" class="form-control my-colorpicker1" name="username" placeholder="Nombre de usuario">
                   </div>
                 </div>
                 <!--Contarseña -->
                 <div class="box-body">
                   <div class="form-group">
                     <label>Contraseña:</label>
-                    <input type="password" class="form-control my-colorpicker1" name="password" placeholder="Contraseña" required>
+                    <input type="password" class="form-control my-colorpicker1" name="password" placeholder="Contraseña">
                   </div>
                 </div>
 
