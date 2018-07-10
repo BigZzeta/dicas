375c7589b516dac980540ebabb7b75fecbd7f3d1
Eric <ericcenteno1@gmail.com>
2018-07-10 17:21:54 -0500

    Actualziando 2

--- resources/views/empleados/empleados.blade.php
+++ resources/views/empleados/empleados.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @extends('inicio')
 @section('content')
 <div class="row">
@@ -49,7 +48,7 @@
                                         <button type="button" onclick="javascript:pasar({{$item->idEmpleado}})" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#borrar" style="margin-left: 3px;" >Borrar</button>
                                     </div>
 
-                                </td> 
+                                </td>
                             </tr>
                             @endforeach
 
@@ -104,7 +103,7 @@
                 </button>
             </div>
             <div class="modal-body"  >
-                <!--Aqui va todo lo chido --> 
+                <!--Aqui va todo lo chido -->
                 <div class="form-group row">
                     <div class="col-md-3"></div>
                     <div class="col-md-6"><!-- mostrar -->
@@ -119,7 +118,7 @@
                         <label class="form-control-label">Código Empleado:</label>
                         <input type="text"  name="codigoEmpleado" class="form-control" value="{{$item->codigoEmpleado}}" readonly>
                     </div>
-                </div> 
+                </div>
                 <div class="row">
                     <div class="col-md-4">
                         <label class="form-control-label">Nombre: </label>
@@ -280,7 +279,7 @@
     elemento.value = id;
     }
 </script>
-<!-- funciones para ajax, envio de datos 
+<!-- funciones para ajax, envio de datos
 <script type="text/javas            cript">
 function ad            d(d) {
 var cerrado = $('#close_add');
@@ -300,274 +299,3 @@ var data = form.serialize();
 
     }
 </script>-->
-=======
-@extends('home')
-@section('content')
-<button class="btn btn-success btn-xl" data-toggle="modal" data-target="#agregar" >Nuevo</button>
-
-<table class="table"  border="3px" >
-
-    <thead  class="text-bold" >
-        <tr>
-            <td>CodigoEmpleado</td>
-            <td>Nombre</td>
-            <td>Apellidos</td>
-            <td>Fotografia</td>
-            <td>FechaNacimiento</td>
-            <td>estadoCivil</td>
-            <td>Sexo</td>
-            <td>Curpi</td>
-            <td>Curpf</td>
-            <td>Num.Seguro S.</td>
-            <td colspan="3" >Acciones</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($empleados as $item)
-        <tr>
-            <td>{{$item->codigoEmpleado}}</td>
-            <td>{{$item->nombre}}</td>
-            <td>{{$item->apellidoPaterno.' '.$item->apellidoMaterno}}</td>
-            <td>{{$item->fotografia}}</td>
-            <td><input type="date" value="{{$item->fechaNacimiento}}" disabled=""/></td>
-            <td>{{$item->estadoCivil}}</td>
-            <td>{{$item->sexo}}</td>
-            <td>{{$item->curpi}}</td>
-            <td>{{$item->curpf}}</td>
-            <td>{{$item->numeroSeguroSocial}}</td>
-
-            <td><button class="btn btn-primary btn-xl" >Ver</button></td>
-            <td><button class="btn btn-file btn-xl" >Actualizar</button></td>
-            <td><button class="btn btn-danger btn-xl" >Eliminar</button></td> 
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-@endSection
-<!- modales bien verijas-->
-<!--modal para insertar nuevos empleados-->
-<div  class="modal fade" id="agregar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
-    <div class="modal-dialog modal-primary modal-xl" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">Añadir nuevo</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form id="add" action="{{route('addEmpleados')}}" method="POST" enctype="multipart/form-data" >
-                    <input id="token" name="_token" value="{{csrf_token()}}" hidden="true"/>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" for="text-input">Codigo Empleado:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="codigoEmpleado" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label">Nombre:</label>
-                        <div class="col-md-9">
-                            <input type="text" name="nombre" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label">Fotografía:</label>
-                        <div class="col-md-9"><!-- mostrar -->
-                            <img src="{{asset('images/usr.png')}}"  height="15%" width="30%"/>
-                            <input type="file" name="fotografia"/>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label">Apellido Paterno:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="apellidoPaterno" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label">Apellido Materno:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="apellidoMaterno" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Nombre Largo:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="nombreLargo" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Fecha Nacimiento:</label>
-                        <div class="col-md-9">
-                            <input type="date"  name="fechaNacimiento" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Lugar Nacimiento:</label>
-                        <div class="col-md-9">
-                            <input type="text" name="lugarNacimiento" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Estado civil:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="estadoCivil" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Sexo:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="sexo" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Curpi:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="curpi" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Curpf:</label>
-                        <div class="col-md-9">
-                            <input type="text" name="curpf" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Numero Seguro Social:</label>
-                        <div class="col-md-9">
-                            <input type="text" name="numeroSeguroSocial" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Umf:</label>
-                        <div class="col-md-9">
-                            <input type="number"  name="umf" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Rfc:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="rfc" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Homoclave :</label>
-                        <div class="col-md-9">
-                            <input type="text" name="homoclave" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Tipo contrato:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="tipoContrato" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Expediente :</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="expediente" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Telefono :</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="telefono" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Código Postal:</label>
-                        <div class="col-md-9">
-                            <input type="number" name="codigoPostal" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Dirección :</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="direccion" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Población:</label>
-                        <div class="col-md-9">
-                            <input type="text" name="poblacion" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Estado :</label>
-                        <div class="col-md-9">
-                            <input type="text" name="estado" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Nombre Padre:</label>
-                        <div class="col-md-9">
-                            <input type="text" name="nombrePadre" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Nombre Madre:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="nombreMadre" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Numero de Afore :</label>
-                        <div class="col-md-9">
-                            <input type="text" name="numeroAfore" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Numero Fonacot :</label>
-                        <div class="col-md-9">
-                            <input type="text" name="NumeroFonacot" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Correo electrónico:</label>
-                        <div class="col-md-9">
-                            <input type="email" name="CorreoElectronico" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Clabe Interbancaria:</label>
-                        <div class="col-md-9">
-                            <input type="text"  name="ClabeInterbancaria" class="form-control" >
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-3 form-control-label" >Entidad Federativa:</label>
-                        <div class="col-md-9">
-                            <input type="text" name="EntidadFederativa" class="form-control" >
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button id="close_add" type="reset" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                <button type="button" onclick="javascript:add('uno')"  class="btn btn-success" >Guardar</button>
-            </div>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-<!--fin del modal que inserta  nuevos empleados-->
-
-<!-- funciones para ajax, envio de datos -->
-<script type="text/javascript">
-    function add(d){
-        var cerrado=$('#close_add');
-        var form=$('#add');
-        var url=form.attr('action');
-        var data=form.serialize();
-        
-        /*Aqui empieza el ajax (lo chido)*/
-        $.post(url,data, function(respuesta){
-            alert(respuesta.msj);
-            if(respuesta.msj=="Guardado correctamente"){
-                  cerrado.click();
-            }
-        }).fail(function(jqXHR){alert('Ha ocurrido un error: '+ jqXHR.responseText);});
-        
-    }
-</script>
->>>>>>> 9d8da7cd819abd81830fc9626734f27c59e19da0
--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,46 @@
 <?php
 
-Route::get('/', 'usuario\UserController@index')->name('inicio');
+Route::get('/', 'usuario\UserController@index')
+  ->name('users');
+
+  Route::get('/dashboard', function(){
+    return view('contenido/dashboard');
+  });
 
 Route::get('/dashboard', function(){
   return view('contenido/dashboard');
 });
 
+//probar esta ruta para el login
+Route::get('/loginx', function(){
+  return view('login');
+});
+
 Route::get('/usuarios', 'usuario\UserController@index')
   ->name('users');
 
-Route::get('/usuarios/nuevo', 'usuario\UserController@create')->name('users.create');
+Route::get('/usuarios/nuevo', 'usuario\UserController@create')
+  ->name('users.create');
 
 Route::post('/usuarios/crear','usuario\UserController@store');
 
-Route::get('/usuarios/{user}/editar', 'usuario\UserController@editar')->name('users.editar');
+Route::get('/usuarios/{users}/editar', 'usuario\UserController@editar')
+  ->name('users.editar');
 
-Route::put('/usuarios/{user}/actualizar','usuario\UserController@update');
+// Route::post('/usuarios/crear','usuario\UserController@store');
+
 
 Route::get('/usuarios/{user}', 'usuario\UserController@show')
   ->where('user', '\w+')
   ->name('users.show');
 
-<<<<<<< HEAD
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-//sadsdsd
-// Route::put('/usuarios/{user}', 'UserController@update');
-// Route::delete('/usuarios/{user}', 'UserController@destroy')->name('users.destroy');
 
 /*empleados*/
 Route::any('/empleados','empleados\EmpleadosController@index')->name('empleados');
 Route::any('/empleados/ver','empleados\EmpleadosController@show')->name('showEmpleados');
 Route::any('/empleados/actualizar/{id?}/{id2?}','empleados\EmpleadosController@update')->name('updateEmpleados');
-=======
-/*empleados*/
-Route::any('/empleados','empleados\EmpleadosController@index')->name('empleados');
-Route::any('/empleados/ver','empleados\EmpleadosController@show')->name('showEmpleados');
-Route::any('/empleados/actualizar/{id?}','empleados\EmpleadosController@update')->name('updateEmpleados');
->>>>>>> 9d8da7cd819abd81830fc9626734f27c59e19da0
 Route::any('/empleados/eliminar/{id?}','empleados\EmpleadosController@delete')->name('deleteEmpleados');
 Route::any('/empleados/agregar/{id?}','empleados\EmpleadosController@add')->name('addEmpleados');
