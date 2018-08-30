0cee2532fbdf0f8d8bce44cc4d09629a1d609ff6
BigZzeta <eleodoro_000@hotmail.com>
2018-08-30 18:04:37 -0500

    merge ->nuevo.blade.php 30-08-2018_6:04 pm

--- resources/views/empleados/nuevo.blade.php
+++ resources/views/empleados/nuevo.blade.php
@@ -27,11 +27,7 @@
                                 <p align="center">
                                     <label class="form-control-label">Fotografía:</label>
                                     <img id="imgSalida" src="{{asset('images/usr.png')}}"  height="15%" width="30%"/>
-<<<<<<< HEAD
                                     <input id="file-input" type="file" name="fotografia"/>  
-=======
-                                    <input id="file-input" type="file" name="fotografia"/>
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 </p>
                             </div>
                         </div>
@@ -41,11 +37,7 @@
                             <div class="col-md-3">
                                 <label class="form-control-label">Codigo Empleado:</label>
                                 <input minlength="4" maxlength="30" type="text"  name="codigoEmpleado" class="form-control text text-uppercase" value="{{ old('codigoEmpleado') }}"  />
-<<<<<<< HEAD
                                 @if($errors->get('codigoEmpleado'))  
-=======
-                                @if($errors->get('codigoEmpleado'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('codigoEmpleado')[0]}}</div>
                                 @endif
                             </div>
@@ -80,24 +72,15 @@
                                     <option value="CASADO (A)"{{old('estadoCivil')=='CASADO (A)' ? 'selected' : ''}} >CASADO (A)</option>
                                     <option value="VIUDO (A)" {{old('estadoCivil')=='VIUDO (A)' ? 'selected' : ''}}>VIUDO (A)</option>
                                     <option value="DIVORCIADO (A)" {{old('estadoCivil')=='DIVORCIADO (A)' ? 'selected' : ''}}>DIVORCIADO (A)</option>
-<<<<<<< HEAD
-=======
-                                    <option value="UNION LIBRE" {{old('estadoCivil')=='UNION LIBRE (A)' ? 'selected' : ''}}>UNION LIBRE</option>
-                                    <option value="OTRO" {{old('estadoCivil')=='OTRO' ? 'selected' : ''}}>OTRO</option>
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
+                                    <option value="UNION LIBRE" {{old('estadoCivil')=='UNION LIBRE' ? 'selected' : ''}}>UNION LIBRE</option>
                                 </select>
                             </div>
                             <div class="col-md-3">
                                 <label class="form-control-label" ><i class="text-danger">*</i> Sexo:</label>
                                 <select name="sexo" class="form-control" required />
                                 <option></option>
-<<<<<<< HEAD
                                 <option  value="MASCULINO" {{old('sexo')=='MASCULINO' ? 'selected' : ''}} >MASCULINO</option>
                                 <option  value="FEMENINO" {{old('sexo')=='FEMENINO' ? 'selected' : ''}} >FEMENINO</option>
-=======
-                                <option label="MASCULINO" value="MASCULINO" {{old('sexo')=='MASCULINO' ? 'selected' : ''}} >MASCULINO </option>
-                                <option label="FEMENINO" value="FEMENINO" {{old('sexo')=='FEMENINO' ? 'selected' : ''}} >FEMENINO </option>
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 </select>
                             </div>
                         </div>
@@ -118,44 +101,28 @@
                             <div class="col-md-4">
                                 <label class=" form-control-label" ><i class="text-danger">*</i> Curp:</label>
                                 <input minlength="18" maxlength="30"   name="curp" class="form-control text text-uppercase" value="{{ old('curp') }}"   required />
-<<<<<<< HEAD
                                 @if($errors->get('curp'))  
-=======
-                                @if($errors->get('curp'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('curp')[0]}}</div>
                                 @endif
                             </div>
                             <div class="col-md-3">
                                 <label class=" form-control-label" ><i class="text-danger">*</i> Rfc:</label>
                                 <input minlength="4" maxlength="12" type="text"  name="rfc" class="form-control text text-uppercase" value="{{ old('rfc') }}" required />
-<<<<<<< HEAD
                                 @if($errors->get('rfc'))  
-=======
-                                @if($errors->get('rfc'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('rfc')[0]}}</div>
                                 @endif
                             </div>
                             <div class="col-md-3">
                                 <label class=" form-control-label" >Numero Seguro Social:</label>
                                 <input maxlength="15" type="text" name="numeroSeguroSocial" class="form-control text text-uppercase" value="{{ old('numeroSeguroSocial') }}" >
-<<<<<<< HEAD
                                 @if($errors->get('numeroSeguroSocial'))  
-=======
-                                @if($errors->get('numeroSeguroSocial'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('numeroSeguroSocial')[0]}}</div>
                                 @endif
                             </div>
                             <div class="col-md-2">
                                 <label class=" form-control-label" >Umf:</label>
                                 <input  maxlength="11"  name="umf" class="form-control text text-uppercase" value="{{ old('umf') }}">
-<<<<<<< HEAD
                                 @if($errors->get('umf'))  
-=======
-                                @if($errors->get('umf'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('umf')[0]}}</div>
                                 @endif
                             </div>
@@ -164,35 +131,21 @@
                             <div class="col-md-3">
                                 <label class=" form-control-label" >Numero de Afore :</label>
                                 <input maxlength="40" type="text" name="numeroAfore" class="form-control text text-uppercase" value="{{ old('numeroAfore') }}" >
-<<<<<<< HEAD
                                 @if($errors->get('numeroAfore'))  
-=======
-                                @if($errors->get('numeroAfore'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('numeroAfore')[0]}}</div>
                                 @endif
                             </div>
                             <div class="col-md-3">
                                 <label class=" form-control-label" >Numero Fonacot :</label>
                                 <input  maxlength="40"  type="text" name="numeroFonacot" class="form-control text text-uppercase" value="{{ old('numeroFonacot') }}">
-<<<<<<< HEAD
                                 @if($errors->get('numeroFonacot'))  
-=======
-                                @if($errors->get('numeroFonacot'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('numeroFonacot')[0]}}</div>
                                 @endif
                             </div>
                             <div class="col-md-3">
-<<<<<<< HEAD
                                 <label class="form-control-label" >Clabe Interbancaria:</label>
                                 <input minlength="4" maxlength="25"  type="text"  name="clabeInterbancaria" class="form-control text text-uppercase" value="{{ old('clabeInterbancaria') }}" />
                                 @if($errors->get('clabeInterbancaria'))  
-=======
-                                <label class="form-control-label" ><i class="text-danger">*</i> Clabe Interbancaria:</label>
-                                <input minlength="4" maxlength="25"  type="text"  name="clabeInterbancaria" class="form-control text text-uppercase" value="{{ old('clabeInterbancaria') }}" required />
-                                @if($errors->get('clabeInterbancaria'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('clabeInterbancaria')[0]}}</div>
                                 @endif
                             </div>
@@ -200,15 +153,12 @@
                                 <label class="form-control-label" ><i class="text-danger">*</i> Tipo contrato:</label>
                                 <select  name="tipoContrato" class="form-control" required />
                                 <option></option>
-<<<<<<< HEAD
                                 <option  value="1" {{old('tipoContrato')==1 ? 'selected' : ''}}>BIMESTRAL</option>
                                 <option  value="2" {{old('tipoContrato')==2 ? 'selected' : ''}}>SEMESTRAL</option>
                                 <option  value="3" {{old('tipoContrato')==3 ? 'selected' : ''}}>ANUAL</option>
-=======
                                 <option  value="1" {{old('tipoContrato')==1 ? 'selected' : ''}}>POR TIEMPO DETERMINADO</option>
                                 <option  value="2" {{old('tipoContrato')==2 ? 'selected' : ''}}>INDETERMINADO</option>
                                 <!-- <option  value="3" {{old('tipoContrato')==3 ? 'selected' : ''}}>POR CAPACITACION</option> -->
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 </select>
                             </div>
                         </div>
@@ -236,11 +186,7 @@
                                 <select name="entidadFederativa" class="form-control" required>
                                     <option></option>
                                     @foreach($estados as $item)
-<<<<<<< HEAD
                                     <option class="form-control" value="{{$item->id}}" {{ old('entidadFederativa')==$item->id ? 'selected' : ''}} >{{$item->estado}}</option>
-=======
-                                    <option class="form-control" value="{{$item->id}}" label="{{$item->estado}}" {{ old('entidadFederativa')==$item->id ? 'selected' : ''}} >{{$item->estado}} </option>
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                     @endforeach
                                 </select>
                             </div>
@@ -258,11 +204,7 @@
                             <div class="col-md-4">
                                 <label class=" form-control-label" ><i class="text-danger">*</i> Correo electrónico:</label>
                                 <input  maxlength="40"  type="email" name="correoElectronico" class="form-control" value="{{ old('correoElectronico') }}" required />
-<<<<<<< HEAD
                                 @if($errors->get('correoElectronico'))  
-=======
-                                @if($errors->get('correoElectronico'))
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
                                 <div class="alert alert-danger">{{ $errors->get('correoElectronico')[0]}}</div>
                                 @endif
                             </div>
@@ -303,7 +245,3 @@
     });
 </script>
 @endsection
-<<<<<<< HEAD
-
-=======
->>>>>>> 774ce2983090cd240c050ac0798b43b8853a118f
